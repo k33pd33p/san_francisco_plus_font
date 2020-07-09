dbe554532e601a168c1def2f65e311a1f8597623
nongthaihoang <nongthaihoang@gmail.com>
2020-07-09 20:09:41 +0700

    code optimization

--- customize.sh
+++ customize.sh
@@ -102,7 +102,7 @@ pixel() {
 				fi
 			fi
 		fi
-		version pxl; PXL=true
+		version pxl & PXL=true
 	fi
 }
 
@@ -111,7 +111,7 @@ oxygen() {
 		set Black Bold Medium Regular Light Thin
 		for i do cp $SYSFONT/$i.ttf $SYSFONT/SlateForOnePlus-$i.ttf; done
 		cp $SYSFONT/Regular.ttf $SYSFONT/SlateForOnePlus-Book.ttf
-		version oos; OOS=true
+		version oos & OOS=true
 	fi
 }
 
@@ -132,7 +132,7 @@ miui() {
 		sed -i '/"mipro-light"/,/family>/{/400/s/MiLanProVF/Light/;/700/s/MiLanProVF/Regular/;/stylevalue/d}' $SYSXML
 		sed -i '/"mipro-normal"/,/family>/{/400/s/MiLanProVF/Light/;/700/s/MiLanProVF/Regular/;/stylevalue/d}' $SYSXML
 		sed -i '/"mipro-regular"/,/family>/{/400/s/MiLanProVF/Regular/;/stylevalue="340"/d}' $SYSXML
-		version miui; MIUI=true
+		version miui & MIUI=true
 	fi
 }
 
@@ -158,7 +158,7 @@ samsung() {
 	if grep -q Samsung $SYSXML; then
 		sed -i 's/SECRobotoLight-//;s/SECCondensed-/Condensed-/' $SYSXML
 		[ $PART -eq 1 ] && sed -i 's/SECRobotoLight-Bold/Medium/' $SYSXML
-		version sam; SAM=true
+		version sam & SAM=true
 	fi
 }
 
