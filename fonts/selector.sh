98ccefa5a2bd5c0853b3b8bde4fef1b9eebfe48d
nongthaihoang <nongthaihoang@gmail.com>
2020-09-13 00:07:55 +0700

    hide volkey in selector

--- fonts/selector.sh
+++ fonts/selector.sh
@@ -1,8 +1,7 @@
 ui_print "  "
 ui_print "- Selector -"
-ui_print "  Volume Key"
-(i=$(getevent -qc 30) && ( ui_print "  ✓"; sleep 0.4 )) & sleep 1; pkill getevent || { . $FONTDIR/volkey.sh; return; }
-ui_print "  Touch Screen"
+i=$(getevent -qc 30) & sleep 0.5; pkill getevent || { . $FONTDIR/volkey.sh; return; }
+ui_print "  Swipe on screen"
 i=$(timeout 2 getevent -qc 30) && { ui_print "  ✓"; sleep 0.4; SEL=selector; } || return
 
 selector() {
